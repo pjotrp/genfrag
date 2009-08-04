b04519221b899abcdfcfbc6eef6ec549ad1f2f4c
Pjotr Prins <pjotr.public01@thebird.nl>
2009-08-04 08:43:30 +0200

    Run with ruby19

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
-../bin/genfrag index -f example.fasta --RE5 BstYI --RE3 MseI --tracktime
-../bin/genfrag search -f example.fasta --RE5 BstYI --RE3 MseI --adapter3 aacc --adapter5 tt --tracktime
+ruby=/home/wrk/.nix-profile/bin/ruby
+$ruby ../bin/genfrag index -f example.fasta --RE5 BstYI --RE3 MseI --tracktime
+$ruby ../bin/genfrag search -f example.fasta --RE5 BstYI --RE3 MseI --adapter3 aacc --adapter5 tt --tracktime
