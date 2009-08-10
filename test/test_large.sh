8cf260b6b78e1190e8ed7f6b3a8a5934583e0da4
Pjotr Prins <pjotr.public01@thebird.nl>
2009-08-10 17:38:32 +0200

    Comment

--- test/test_large.sh
+++ test/test_large.sh
@@ -3,4 +3,5 @@
 genfrag=/home/wrk/izip/git/opensource/genfrag/bin/genfrag
 fn=large.fasta
 $genfrag index $* -f $fn --RE5 BstYI --RE3 MseI --tracktime > test_index.log
+# ruby -rprofile $genfrag search $* -f $fn --RE5 BstYI --RE3 MseI --adapter3 aa --adapter5 tt --tracktime > test_search.log
 $genfrag search $* -f $fn --RE5 BstYI --RE3 MseI --adapter3 aa --adapter5 tt --tracktime > test_search.log
