1d5157701a394055fb5cb258f8abce4d8f96aea5
Pjotr Prins <pjotr.public01@thebird.nl>
2009-08-20 08:12:22 +0200

    Profiler scripts

---
+++ test/test_large_index.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+genfrag=../bin/genfrag
+fn=large.fasta
+$genfrag index $* -f $fn --RE5 BstYI --RE3 MseI --tracktime > test_index.log
+# ruby -rprofile $genfrag search $* -f $fn --RE5 BstYI --RE3 MseI --adapter3 aa --adapter5 tt --tracktime > test_search.log
+# $genfrag search $* -f $fn --RE5 BstYI --RE3 MseI --adapter3 aa --adapter5 tt --tracktime > test_search.log
--- test/test_large_search.rb
+++ test/test_large_search.rb
@@ -1,7 +1,6 @@
 #! /bin/sh
 
-genfrag=/home/wrk/izip/git/opensource/genfrag/bin/genfrag
+genfrag=../bin/genfrag
 fn=large.fasta
-$genfrag index $* -f $fn --RE5 BstYI --RE3 MseI --tracktime > test_index.log
 # ruby -rprofile $genfrag search $* -f $fn --RE5 BstYI --RE3 MseI --adapter3 aa --adapter5 tt --tracktime > test_search.log
 $genfrag search $* -f $fn --RE5 BstYI --RE3 MseI --adapter3 aa --adapter5 tt --tracktime > test_search.log
